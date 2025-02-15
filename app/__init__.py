05442cd5f9cbb6d6e91efdf97caedcf706d55d2b
minato <lock.extinguish@gmail.com>
2025-02-15 15:18:18 +0900

    app.routeの削除

--- app/__init__.py
+++ app/__init__.py
@@ -25,10 +25,5 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
     app.register_blueprint(users_bp)
     return app
