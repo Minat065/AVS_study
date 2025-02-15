475de8b97e785430cb29ff2b2af6274099ab40a9
kanahash <kanahash@Cerna-NEC>
2025-02-15 16:17:26 +0900

    init_database

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask
 from app.views.user_views import users_bp
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 def create_app(test_config=None):
@@ -9,15 +11,15 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
     )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    # if test_config is None:
+    #     # load the instance config, if it exists, when not testing
+    #     app.config.from_pyfile('config.py', silent=True)
+    # else:
+    #     # load the test config if passed in
+    #     app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
     try:
@@ -26,4 +28,10 @@ def create_app(test_config=None):
         pass
 
     app.register_blueprint(users_bp)
+
+  
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
+
     return app
